7874a7e3124970baa9dcf26f353da402a409ddc6
程嘉梁 <chengjl16@163.com>
2018-10-10 16:27:39 +0800

    初步实现牌组和整套牌的功能

--- Landlords/main.asm
+++ Landlords/main.asm
@@ -7,7 +7,18 @@
 TITLE Main (main.asm)
 
 ;引用文件
-include template.inc
+include irvine32.inc
+includelib irvine32.lib
+
+include cardgroup.inc
+include cards.inc
+INCLUDE Macros.inc
+
+
+.data
+	group1 CardGroup <>
+	group2 CardGroup <>
+	result BYTE ?
 
 .code
 
@@ -17,7 +28,28 @@ Main PROC
 ; 返回值:无
 ;-----------------------------------------------------
 	;函数实现
-	invoke Temp,0,0
+		
+	invoke AddNumber,ADDR group2,18
+	invoke AddNumber,ADDR group2,31
+	invoke AddNumber,ADDR group2,5
+	invoke AddNumber,ADDR group2,6
+
+	invoke DeleteNumber,ADDR group2,31
+	invoke DeleteNumber,ADDR group2,3
+
+	;invoke Clear,ADDR group2
+
+	mDumpMem OFFSET group2.count, LENGTHOF group2.count, TYPE group2.count
+	mDumpMem OFFSET group2.card_group, LENGTHOF group2.card_group, TYPE group2.card_group
+	mDumpMem OFFSET group2.cards, LENGTHOF group2.cards, TYPE group2.cards
+
+	invoke Cards,ADDR cards_cards,ADDR cards_remain
+	invoke GetCard,ADDR result,ADDR cards_remain,ADDR cards_cards
+	mDumpMem OFFSET cards_cards, LENGTHOF cards_cards, TYPE cards_cards
+	mDumpMem OFFSET cards_remain, LENGTHOF cards_remain, TYPE cards_remain
+	mDumpMem OFFSET result, LENGTHOF result, TYPE result
+
+
 	ret
 Main ENDP
 
